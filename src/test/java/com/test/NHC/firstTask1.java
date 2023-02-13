82c2de2efabbf80023c9a3607e45302b79463092
Diusupova <jamasultan2015@gmail.com>
2023-02-12 23:51:20 -0500

    adding file

--- src/test/java/com/test/NHC/firstTask1.java
+++ src/test/java/com/test/NHC/firstTask1.java
@@ -9,15 +9,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class FirstTask {
+public class firstTask1 {
     @Test
     public void nhcTest() throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.navigate().to(ConfigReader.readProperty("nhcUrl"));
-       WebElement login = driver.findElement(By.id("inputEmail"));
-       login.sendKeys(ConfigReader.readProperty("nhcLogin"));
+        WebElement login = driver.findElement(By.id("inputEmail"));
+        login.sendKeys(ConfigReader.readProperty("nhcLogin"));
         WebElement password = driver.findElement(By.id("inputPassword"));
         password.sendKeys(ConfigReader.readProperty("nhcPassword"));
         Thread.sleep(3000);
@@ -33,3 +33,4 @@ public class FirstTask {
 
     }
 }
+
